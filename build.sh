7f1c68d040bb191fb1e027f484e89b1d4be97023
Kevin Nobel <git@2sk.nl>
2023-01-07 22:37:29 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Enable double asterisk (**) in bash
+shopt -s globstar
+
+# Download RetroMCP
+wget https://github.com/MCPHackers/RetroMCP-Java/releases/download/v1.0-pre3/RetroMCP-Java-all.jar -O RetroMCP.jar
+
+# Initialize RetroMCP
+java -jar RetroMCP.jar cleanup
+java -jar RetroMCP.jar setup b1.7.3
+java -jar RetroMCP.jar decompile
+
+# Apply the patches
+git apply --verbose --directory=src/minecraft patches/patches_client/**/*.patch
+
+# Build the minecraft zip
+java -jar RetroMCP.jar build
+
+# Copy the zip to the project root
+cp build/minecraft.zip BetaFix.zip
