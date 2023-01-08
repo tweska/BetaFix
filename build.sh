777c2e150a294fccd6d725531dfa2cf4cbec7e54
Kevin Nobel <git@2sk.nl>
2023-01-08 14:19:52 +0100

    Target build script on client only

--- build.sh
+++ build.sh
@@ -9,13 +9,13 @@ wget https://github.com/MCPHackers/RetroMCP-Java/releases/download/v1.0-pre3/Ret
 # Initialize RetroMCP
 java -jar RetroMCP.jar cleanup
 java -jar RetroMCP.jar setup b1.7.3
-java -jar RetroMCP.jar decompile
+java -jar RetroMCP.jar decompile client
 
 # Apply the patches
 git apply --verbose --directory=src/minecraft patches/patches_client/**/*.patch
 
 # Build the minecraft zip
-java -jar RetroMCP.jar build
+java -jar RetroMCP.jar build client
 
 # Copy the zip to the project root
 cp build/minecraft.zip BetaFix.zip
